6561ab5df1b28faa45c9f40f667c21f88e282660
Max Bureck <max.bureck@fokus.fraunhofer.de>
2014-01-16 19:08:57 +0100

    First test project

---
+++ tests/org.eclipse.upr.depl.tests/src/org/eclipse/upr/depl/tests/ComponentAssembly.java
@@ -0,0 +1,14 @@
+package org.eclipse.upr.depl.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComponentAssembly {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
